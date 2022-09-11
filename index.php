6ab01cff66c432415e621fb52887fcdbebb262ad
Rostyslawn <75090630+Rostyslawn@users.noreply.github.com>
2022-09-11 14:28:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+﻿<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="https://hotmax.com.ua/assets/images/stories/map.jpg" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header block">
+        <nav class="nav">
+            <a><img class="logo" alt="logo" src="imgs/Cuda.png"></a>
+            <ul class="ul">
+                <li>Home</li>
+                <li>About</li>
+                <li>Work</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <div class="text">
+            <p>
+                Hi there! We are the new kids on the block and we build awesome
+                websites and mobile apps.
+            </p>
+            <button class="button">WORK WITH US!</button>
+        </div>
+    </div>
+    <div class="services">
+        <div class="title">
+            <h2 class="h2">SERVICES WE PROVIDE</h2>
+            <div class="hr"></div>
+            <p class="text">
+                We are working with both individual and business fromall over the
+                globe to create awesome websites and applications.
+            </p>
+        </div>
+        <ul>
+            <li>
+                <img src="imgs/branding.png">
+                <h3>BRANDING</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur omnis explicabo alias.
+                </p>
+            </li>
+            <li>
+                <img src="imgs/design.png">
+                <h3>DESIGN</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    Consectetur omnis explicabo alias.
+                </p>
+            </li>
+            <li>
+                <img src="imgs/development.png">
+                <h3>DEVELOPMENT</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    Consectetur omnis explicabo alias.
+                </p>
+            </li>
+            <li>
+                <img src="imgs/rocketscience.png">
+                <h3>ROCKET SCIENCE</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur omnis explicabo alias.
+                </p>
+            </li>
+        </ul>
+    </div>
+    <div class="team">
+        <div class="title">
+            <h2>MEET OUR BEAUTIFUL TEAM</h2>
+            <div class="hr"></div>
+            <p class="text">
+                We are a small team of designers and developers, who help brands
+                with big ideas.
+            </p>
+        </div>
+        <ul class="ul">
+            <li>
+                <img src="imgs/oval.png">
+                <h3 class="h3">ANNE HATHAWAY</h3>
+                <h4>CEO / Marketing Guru</h4>
+                <p class="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis saepe optio ut minima veniam quas debitis delectus, ea quo mollitia.</p>
+                <ul class="networks">
+                    <li><img class="email" src="imgs/facebook.png"></li>
+                    <li><img class="email" src="imgs/twitter.png"></li>
+                    <li><img class="email" src="imgs/what.png"></li>
+                    <li><img class="email" src="imgs/email.png"></li>
+                </ul>
+            </li>
+            <li>
+                <img src="imgs/oval.png">
+                <h3 class="h3">KATE UPTON</h3>
+                <h4>Creative Director</h4>
+                <p class="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis saepe optio ut minima veniam quas debitis delectus, ea quo mollitia.</p>
+                <ul class="networks">
+                    <li><img class="email" src="imgs/twitter.png"></li>
+                    <li><img class="email" src="imgs/what.png"></li>
+                    <li><img class="email" src="imgs/email.png"></li>
+                </ul>
+            </li>
+            <li>
+                <img src="imgs/oval.png">
+                <h3 class="h3">OLIVIA WILDE</h3>
+                <h4>Lead Designer</h4>
+                <p class="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis saepe optio ut minima veniam quas debitis delectus, ea quo mollitia.</p>
+                <ul class="networks">
+                    <li><img class="email" src="imgs/facebook.png"></li>
+                    <li><img class="email" src="imgs/twitter.png"></li>
+                    <li><img class="email" src="imgs/what.png"></li>
+                    <li><img class="email" src="imgs/email.png"></li>
+                </ul>
+            </li>
+            <li>
+                <img src="imgs/oval.png">
+                <h3 class="h3">ASHLEY GREENE</h3>
+                <h4>SEO / Developer</h4>
+                <p class="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis saepe optio ut minima veniam quas debitis delectus, ea quo mollitia.</p>
+                <ul class="networks">
+                    <li><img class="email" src="imgs/facebook.png"></li>
+                    <li><img class="email" src="imgs/twitter.png"></li>
+                    <li><img class="email" src="imgs/what.png"></li>
+                    <li><img class="email" src="imgs/email.png"></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="skills">
+        <div class="title">
+            <h2>WE GOT SKILLS!</h2>
+            <div class="hr"></div>
+            <p class="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, quod
+                magnam aut consectetur dicta iusto ad voluptate minus ipsam
+                corrupti.
+            </p>
+        </div>
+        <ul class="skill-list">
+            <li>
+                <img class="email" src="imgs/90.png">
+                <h3>WEB DESIGN</h3>
+            </li>
+            <li>
+                <img class="email" src="imgs/75.png">
+                <h3>HTML/CSS</h3>
+            </li>
+            <li>
+                <img class="email" src="imgs/70.png">
+                <h3>GRAPHIC DESIGN</h3>
+            </li>
+            <li>
+                <img class="email" src="imgs/85.png">
+                <h3>UI/UX</h3>
+            </li>
+        </ul>
+    </div>
+    <div class="portfilio">
+        <div class="title">
+            <h2>OUR PORTFOLIO</h2>
+            <div class="hr"></div>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+                commodi, alias a possimus eligendi voluptatum vitae laboriosam ullam
+                quibusdam accusantium accusamus.
+            </p>
+            <ul>
+                <li class="active">All</li>
+                <li>Web</li>
+                <li>App</li>
+                <li>Icon</li>
+            </ul>
+        </div>
+        <div class="works section">
+            <ul>
+                <li><img src="imgs/isometric.png">Isometric Perspective Mock-up</li>
+                <li><img src="imgs/timezone.png">Time Zone App Ui</li>
+                <li><img src="imgs/viromedia.png">Viro Media players ui</li>
+                <li><img src="imgs/blog.png">Blog / magazine flat ui kit</li>
+            </ul>
+        </div>
+        <button class="button">LOAD MORE PROJECTS</button>
+    </div>
+    <div class="abouts">
+        <div class="title">
+            <h2>WHAT PEOPLE SAY ABOUT US</h2>
+            <div class="hr"></div>
+            <p class="text">Our clients love us!</p>
+        </div>
+        <ul class="clients section">
+            <li>
+                <img src="imgs/oval.png">
+                <div class="text">
+                    <p>
+                        “Nullam dapibus blandit orci, viverra gravida dui lobortis eget.
+                        Maecenas fringilla urna eu nisl scelerisque.”
+                    </p>
+                    <h3>Chanel Iman</h3>
+                    <h4>CEO of Pinterest</h4>
+                </div>
+            </li>
+            <li>
+                <img src="imgs/oval.png">
+                <div class="text">
+                    <p>
+                        “Nullam dapibus blandit orci, viverra gravida dui lobortis eget.
+                        Maecenas fringilla urna eu nisl scelerisque.”
+                    </p>
+                    <h3>Chanel Iman</h3>
+                    <h4>CEO of Pinterest</h4>
+                </div>
+            </li>
+            <li>
+                <img src="imgs/oval.png">
+                <div class="text">
+                    <p>
+                        “Nullam dapibus blandit orci, viverra gravida dui lobortis eget.
+                        Maecenas fringilla urna eu nisl scelerisque.”
+                    </p>
+                    <h3>Chanel Iman</h3>
+                    <h4>CEO of Pinterest</h4>
+                </div>
+            </li>
+            <li>
+                <img src="imgs/oval.png">
+                <div class="text">
+                    <p>
+                        “Nullam dapibus blandit orci, viverra gravida dui lobortis eget.
+                        Maecenas fringilla urna eu nisl scelerisque.”
+                    </p>
+                    <h3>Chanel Iman</h3>
+                    <h4>CEO of Pinterest</h4>
+                </div>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
